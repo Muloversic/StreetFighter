6b2989d3f5677c60213125e34a83bbb8abe58e5e
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-06-08 20:37:17 +0300

    player one and two can't attack when they block

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,25 +1,92 @@
 import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
-  window.addEventListener('keydown', (e) => {
-    if (e.code === controls.PlayerOneAttack) {
-      // first player attacks
-      getDamage(firstFighter, null);
-    }
+  runOnKeys(
+    (pressed) => {
+      if (pressed.has(controls.PlayerOneAttack) && !pressed.has(controls.PlayerOneBlock)) {
+        // first player attacks
+        const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+        [...playerNameElements].forEach((playerNameElement) => {
+          if (playerNameElement.textContent === secondFighter.name) {
+            const healtBarElement = playerNameElement.nextElementSibling.children[0];
+            secondFighter.health -= getDamage(firstFighter, null);
+            healtBarElement.style.width = secondFighter.health + '%';
+            if (secondFighter.health <= 0) {
+              healtBarElement.style.width = 0 + '%';
+              secondFighter.health = 0;
+              return;
+            }
+          }
+        });
+      }
+      if (pressed.has(controls.PlayerTwoAttack) && !pressed.has(controls.PlayerTwoBlock)) {
+        // second player attacks
+        const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+        [...playerNameElements].forEach((playerNameElement) => {
+          if (playerNameElement.textContent === firstFighter.name) {
+            const healtBarElement = playerNameElement.nextElementSibling.children[0];
+            firstFighter.health -= getDamage(secondFighter, null);
+            healtBarElement.style.width = firstFighter.health + '%';
+            if (firstFighter.health <= 0) {
+              healtBarElement.style.width = 0 + '%';
+              firstFighter.health = 0;
+              return;
+            }
+          }
+        });
+      }
+    },
+    controls.PlayerOneAttack,
+    controls.PlayerTwoAttack,
+    controls.PlayerOneBlock,
+    controls.PlayerTwoBlock
+  );
 
-    if (e.code === controls.PlayerOneBlock) {
+  window.addEventListener('keydown', (e) => {
+    // if (e.code === controls.PlayerOneAttack && e.code !== controls.PlayerOneBlock) {
+    //   // first player attacks
+    //   const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+    //   [...playerNameElements].forEach((playerNameElement) => {
+    //     if (playerNameElement.textContent === secondFighter.name) {
+    //       const healtBarElement = playerNameElement.nextElementSibling.children[0];
+    //       secondFighter.health -= getDamage(firstFighter, null);
+    //       healtBarElement.style.width = secondFighter.health + '%';
+    //       if (secondFighter.health <= 0) {
+    //         healtBarElement.style.width = 0 + '%';
+    //         secondFighter.health = 0;
+    //         return;
+    //       }
+    //     }
+    //   });
+    // }
+
+    if (e.code === controls.PlayerOneBlock && e.code === controls.PlayerTwoAttack) {
       // first player defends
-      getDamage(null, firstFighter);
+      //   console.log(e.code);
+      //   getDamage(null, firstFighter);
     }
 
     if (e.code === controls.PlayerTwoAttack) {
       //  second attacks
-      getDamage(secondFighter, null);
+      //   const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+      //   [...playerNameElements].forEach((playerNameElement) => {
+      //     if (playerNameElement.textContent === firstFighter.name) {
+      //       const healtBarElement = playerNameElement.nextElementSibling.children[0];
+      //       firstFighter.health -= getDamage(secondFighter, null);
+      //       healtBarElement.style.width = firstFighter.health + '%';
+      //       if (firstFighter.health <= 0) {
+      //         healtBarElement.style.width = 0 + '%';
+      //         firstFighter.health = 0;
+      //         return;
+      //       }
+      //     }
+      //   });
     }
 
-    if (e.code === controls.PlayerTwoBlock) {
+    if (e.code === controls.PlayerTwoBlock && e.code === controls.PlayerOneAttack) {
       // second defends
-      getDamage(null, secondFighter);
+      //   getDamage(null, secondFighter);
+      console.log('player one attack, second block');
     }
   });
 
@@ -36,12 +103,16 @@ export function getDamage(attacker, defender) {
   if (defender) {
     return getBlockPower(defender);
   }
+
+  if (getBlockPower(defender) > getHitPower(attacker)) {
+    return 0;
+  }
 }
 
 export function getHitPower(fighter) {
   const criticalHitChance = Math.random() * (3 - 1) + 1; // chance from 1 to 2
   const power = fighter.attack * criticalHitChance;
-  console.log('attac', fighter);
+  //   console.log('attac', fighter);
   // return hit power
   return power;
 }
@@ -49,7 +120,32 @@ export function getHitPower(fighter) {
 export function getBlockPower(fighter) {
   const dodgeChance = Math.random() * (3 - 1) + 1; // chance from 1 to 2
   const power = fighter.defense * dodgeChance;
-  console.log('defence', fighter);
+  //   console.log('defence', fighter);
   // return block power
   return power;
+}
+
+function runOnKeys(func, ...codes) {
+  let pressed = new Set();
+
+  document.addEventListener('keydown', function (event) {
+    pressed.add(event.code);
+    // for (let code of codes) {
+    //   // все ли клавиши из набора нажаты?
+    //   if (!pressed.has(code)) {
+    //     return;
+    //   }
+    // }
+
+    // да, все
+    // во время показа alert, если посетитель отпустит клавиши - не возникнет keyup
+    // при этом JavaScript "пропустит" факт отпускания клавиш, а pressed[keyCode] останется true
+    // чтобы избежать "залипания" клавиши -- обнуляем статус всех клавиш, пусть нажимает всё заново
+    // pressed.clear();
+    func(pressed);
+  });
+
+  document.addEventListener('keyup', function (event) {
+    pressed.delete(event.code);
+  });
 }
