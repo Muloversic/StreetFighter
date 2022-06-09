1487236b7aec744cac4a0fe2ef5275e1cfe66c57
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-06-09 16:41:17 +0300

    players can do a critical damage

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,19 +1,100 @@
+import { doc } from 'prettier';
 import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
-  runOnKeys(
-    (pressed) => {
-      if (
-        pressed.has(controls.PlayerOneAttack) &&
-        !pressed.has(controls.PlayerOneBlock) &&
-        !pressed.has(controls.PlayerTwoBlock)
-      ) {
-        // first player attacks
+  let isAllowedPlayerOneToCrit = true;
+  let isAllowedPlayerTwoToCrit = true;
+  runOnKeys((pressed) => {
+    if (
+      pressed.has(controls.PlayerOneAttack) &&
+      !pressed.has(controls.PlayerOneBlock) &&
+      !pressed.has(controls.PlayerTwoBlock)
+    ) {
+      // first player attacks
+      const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+      [...playerNameElements].forEach((playerNameElement) => {
+        if (playerNameElement.textContent === secondFighter.name) {
+          const healtBarElement = playerNameElement.nextElementSibling.children[0];
+          secondFighter.health -= getDamage(firstFighter, null);
+          healtBarElement.style.width = secondFighter.health + '%';
+          if (secondFighter.health <= 0) {
+            healtBarElement.style.width = 0 + '%';
+            secondFighter.health = 0;
+            return;
+          }
+        }
+      });
+    }
+
+    if (pressed.has(controls.PlayerOneAttack) && pressed.has(controls.PlayerTwoBlock)) {
+      // first player attacks
+      const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+      [...playerNameElements].forEach((playerNameElement) => {
+        if (playerNameElement.textContent === secondFighter.name) {
+          const healtBarElement = playerNameElement.nextElementSibling.children[0];
+          secondFighter.health -= getDamage(firstFighter, null) - getDamage(null, secondFighter);
+          healtBarElement.style.width = secondFighter.health + '%';
+          if (secondFighter.health <= 0) {
+            healtBarElement.style.width = 0 + '%';
+            secondFighter.health = 0;
+            return;
+          }
+        }
+      });
+    }
+
+    if (
+      pressed.has(controls.PlayerTwoAttack) &&
+      !pressed.has(controls.PlayerTwoBlock) &&
+      !pressed.has(controls.PlayerOneBlock)
+    ) {
+      // second player attacks
+      const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+      [...playerNameElements].forEach((playerNameElement) => {
+        if (playerNameElement.textContent === firstFighter.name) {
+          const healtBarElement = playerNameElement.nextElementSibling.children[0];
+          firstFighter.health -= getDamage(secondFighter, null);
+          console.log('clear damage', getDamage(secondFighter, null));
+          healtBarElement.style.width = firstFighter.health + '%';
+          if (firstFighter.health <= 0) {
+            healtBarElement.style.width = 0 + '%';
+            firstFighter.health = 0;
+            return;
+          }
+        }
+      });
+    }
+
+    if (pressed.has(controls.PlayerTwoAttack) && pressed.has(controls.PlayerOneBlock)) {
+      // second player attacks
+      const playerNameElements = document.querySelectorAll('.arena___fighter-name');
+      [...playerNameElements].forEach((playerNameElement) => {
+        if (playerNameElement.textContent === firstFighter.name) {
+          const healtBarElement = playerNameElement.nextElementSibling.children[0];
+          firstFighter.health -= getDamage(secondFighter, null) - getDamage(null, firstFighter);
+          console.log('blocked damage', getDamage(secondFighter, null) - getDamage(null, firstFighter));
+
+          healtBarElement.style.width = firstFighter.health + '%';
+          if (firstFighter.health <= 0) {
+            healtBarElement.style.width = 0 + '%';
+            firstFighter.health = 0;
+            return;
+          }
+        }
+      });
+    }
+
+    const didPlayerOneCriticalCombination = controls.PlayerOneCriticalHitCombination.every((key) => pressed.has(key));
+    const didPlayerTwoCriticalCombination = controls.PlayerTwoCriticalHitCombination.every((key) => pressed.has(key));
+
+    if (isAllowedPlayerOneToCrit) {
+      if (didPlayerOneCriticalCombination) {
+        isAllowedPlayerOneToCrit = false;
         const playerNameElements = document.querySelectorAll('.arena___fighter-name');
         [...playerNameElements].forEach((playerNameElement) => {
           if (playerNameElement.textContent === secondFighter.name) {
             const healtBarElement = playerNameElement.nextElementSibling.children[0];
-            secondFighter.health -= getDamage(firstFighter, null);
+            secondFighter.health -= getDamage(firstFighter, null) * 2;
             healtBarElement.style.width = secondFighter.health + '%';
             if (secondFighter.health <= 0) {
               healtBarElement.style.width = 0 + '%';
@@ -22,37 +103,22 @@ export async function fight(firstFighter, secondFighter) {
             }
           }
         });
-      }
 
-      if (pressed.has(controls.PlayerOneAttack) && pressed.has(controls.PlayerTwoBlock)) {
-        // first player attacks
-        const playerNameElements = document.querySelectorAll('.arena___fighter-name');
-        [...playerNameElements].forEach((playerNameElement) => {
-          if (playerNameElement.textContent === secondFighter.name) {
-            const healtBarElement = playerNameElement.nextElementSibling.children[0];
-            secondFighter.health -= getDamage(firstFighter, null) - getDamage(null, secondFighter);
-            healtBarElement.style.width = secondFighter.health + '%';
-            if (secondFighter.health <= 0) {
-              healtBarElement.style.width = 0 + '%';
-              secondFighter.health = 0;
-              return;
-            }
-          }
-        });
+        pressed.clear();
+        setTimeout(() => {
+          isAllowedPlayerOneToCrit = true;
+        }, 10000);
       }
+    }
 
-      if (
-        pressed.has(controls.PlayerTwoAttack) &&
-        !pressed.has(controls.PlayerTwoBlock) &&
-        !pressed.has(controls.PlayerOneBlock)
-      ) {
-        // second player attacks
+    if (isAllowedPlayerTwoToCrit) {
+      if (didPlayerTwoCriticalCombination) {
+        isAllowedPlayerTwoToCrit = false;
         const playerNameElements = document.querySelectorAll('.arena___fighter-name');
         [...playerNameElements].forEach((playerNameElement) => {
           if (playerNameElement.textContent === firstFighter.name) {
             const healtBarElement = playerNameElement.nextElementSibling.children[0];
-            firstFighter.health -= getDamage(secondFighter, null);
-            console.log('clear damage', getDamage(secondFighter, null));
+            firstFighter.health -= getDamage(secondFighter, null) * 2;
             healtBarElement.style.width = firstFighter.health + '%';
             if (firstFighter.health <= 0) {
               healtBarElement.style.width = 0 + '%';
@@ -61,32 +127,14 @@ export async function fight(firstFighter, secondFighter) {
             }
           }
         });
-      }
-	  
-      if (pressed.has(controls.PlayerTwoAttack) && pressed.has(controls.PlayerOneBlock)) {
-        // second player attacks
-        const playerNameElements = document.querySelectorAll('.arena___fighter-name');
-        [...playerNameElements].forEach((playerNameElement) => {
-          if (playerNameElement.textContent === firstFighter.name) {
-            const healtBarElement = playerNameElement.nextElementSibling.children[0];
-            firstFighter.health -= getDamage(secondFighter, null) - getDamage(null, firstFighter);
-            console.log('blocked damage', getDamage(secondFighter, null) - getDamage(null, firstFighter));
 
-            healtBarElement.style.width = firstFighter.health + '%';
-            if (firstFighter.health <= 0) {
-              healtBarElement.style.width = 0 + '%';
-              firstFighter.health = 0;
-              return;
-            }
-          }
-        });
+        pressed.clear();
+        setTimeout(() => {
+          isAllowedPlayerTwoToCrit = true;
+        }, 10000);
       }
-    },
-    controls.PlayerOneAttack,
-    controls.PlayerTwoAttack,
-    controls.PlayerOneBlock,
-    controls.PlayerTwoBlock
-  );
+    }
+  });
 
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
@@ -109,7 +157,7 @@ export function getDamage(attacker, defender) {
 
 export function getHitPower(fighter) {
   const criticalHitChance = Math.random() * (3 - 1) + 1; // chance from 1 to 2
-  const power = fighter.attack * 1;
+  const power = fighter.attack * criticalHitChance;
   //   console.log('attac', fighter);
   // return hit power
   return power;
@@ -117,16 +165,17 @@ export function getHitPower(fighter) {
 
 export function getBlockPower(fighter) {
   const dodgeChance = Math.random() * (3 - 1) + 1; // chance from 1 to 2
-  const power = fighter.defense * 1;
+  const power = fighter.defense * dodgeChance;
   //   console.log('defence', fighter);
   // return block power
   return power;
 }
 
-function runOnKeys(func, ...codes) {
+function runOnKeys(func) {
   let pressed = new Set();
 
   document.addEventListener('keydown', function (event) {
+    event.preventDefault();
     pressed.add(event.code);
     // for (let code of codes) {
     //   // все ли клавиши из набора нажаты?
@@ -139,7 +188,7 @@ function runOnKeys(func, ...codes) {
     // во время показа alert, если посетитель отпустит клавиши - не возникнет keyup
     // при этом JavaScript "пропустит" факт отпускания клавиш, а pressed[keyCode] останется true
     // чтобы избежать "залипания" клавиши -- обнуляем статус всех клавиш, пусть нажимает всё заново
-    // pressed.clear();
+
     func(pressed);
   });
 
